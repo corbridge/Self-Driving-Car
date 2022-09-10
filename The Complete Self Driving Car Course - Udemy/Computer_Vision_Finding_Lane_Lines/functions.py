2f90ae68f0dda8e935d0e44e0c5213b899ada47d
Cornelio Posadas P <posadascornelio38@gmail.com>
2022-09-09 23:42:09 -0500

    add functions file

---
+++ The Complete Self Driving Car Course - Udemy/Computer_Vision_Finding_Lane_Lines/functions.py
@@ -0,0 +1,8 @@
+import cv2
+import numpy as np
+
+def canny(lane_image):
+    gray = cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY)
+    blur = cv2.GaussianBlur(gray, (5,5),0)
+    canny = cv2.Canny(blur, 50, 150)
+    return canny
