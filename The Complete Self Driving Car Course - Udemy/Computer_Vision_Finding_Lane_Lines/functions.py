c90e853a20bdabab4ddeefd0cf79f543cb2aaa96
Cornelio Posadas P <posadascornelio38@gmail.com>
2022-09-10 02:01:05 -0500

    make lines better

--- The Complete Self Driving Car Course - Udemy/Computer_Vision_Finding_Lane_Lines/functions.py
+++ The Complete Self Driving Car Course - Udemy/Computer_Vision_Finding_Lane_Lines/functions.py
@@ -21,12 +21,47 @@ def region_of_interest(image):
 
 def display_lines(image, lines):
 
+    color = (0,255,0)
+    thick = 10
+
     line_image = np.zeros_like(image)
     if lines is not None:
         for line in lines:
             x1,y1,x2,y2 = line.reshape(4)
-            cv2.line(line_image, (x1,y1),(x2,y2), (0,255,0), 10)
+            cv2.line(line_image, (x1,y1),(x2,y2), color, thick)
     return line_image
+
+def average_slope_intercept(image, lines):
+    
+    left_fit = []
+    right_fit = []
+
+    for line in lines:
+        x1,y1,x2,y2 = line.reshape(4)
+        parameters = np.polyfit((x1,x2), (y1,y2), 1)
+        slope = parameters[0]
+        intercept = parameters[1]
+
+        if slope < 0:
+            left_fit.append((slope, intercept))
+        else:
+            right_fit.append((slope, intercept))
+    
+    left_fit_average = np.average(left_fit, axis = 0)
+    right_fit_average = np.average(right_fit,  axis = 0)
+
+    left_line = make_coordinates(image, left_fit_average)
+    right_line = make_coordinates(image, right_fit_average)
+    return np.array([left_line, right_line])
+
+def make_coordinates(image, line_parameters):
+
+    slope, intercept = line_parameters
+    y1 = image.shape[0]
+    y2 = int(y1 * (3/5))
+    x1 = int((y1 - intercept) / slope)
+    x2 = int((y2 - intercept) / slope)
+    return np.array([x1,y1,x2,y2])
         
 
 
--- The Complete Self Driving Car Course - Udemy/Computer_Vision_Finding_Lane_Lines/image.py
+++ The Complete Self Driving Car Course - Udemy/Computer_Vision_Finding_Lane_Lines/image.py
@@ -7,7 +7,8 @@ image = cv2.imread(r"C:\Users\DELL\Desktop\Cursos\The self driving car course\Th
 edges = canny(image)
 cropped_image = region_of_interest(edges)
 line = cv2.HoughLinesP(cropped_image, 2, np.pi/180, 100, np.array([]), minLineLength = 40, maxLineGap = 5)
-line_image = display_lines(image, line)
+average_line = average_slope_intercept(image, line)
+line_image = display_lines(image, average_line)
 merge_image = cv2.addWeighted(image, 0.8, line_image, 1,1)
 
 cv2.imshow('Result', merge_image)
