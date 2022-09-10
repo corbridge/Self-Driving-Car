216428b62ef8515f27bfa5632b4d1b42b79eba40
Cornelio Posadas P <posadascornelio38@gmail.com>
2022-09-10 00:13:06 -0500

    update region of interest (triangule)

--- The Complete Self Driving Car Course - Udemy/Computer_Vision_Finding_Lane_Lines/functions.py
+++ The Complete Self Driving Car Course - Udemy/Computer_Vision_Finding_Lane_Lines/functions.py
@@ -1,8 +1,17 @@
+from email.mime import image
 import cv2
 import numpy as np
+import matplotlib as plt
 
-def canny(lane_image):
-    gray = cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY)
+def canny(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
     blur = cv2.GaussianBlur(gray, (5,5),0)
     canny = cv2.Canny(blur, 50, 150)
     return canny
+
+def region_of_interest(image):
+    height = image.shape[0]
+    triangule = np.array([(200, height),(1100, height), (550,250)], dtype=np.int64)
+    mask = np.zeros_like(image)
+    cv2.fillPoly(mask, [triangule], 255)
+    return mask
