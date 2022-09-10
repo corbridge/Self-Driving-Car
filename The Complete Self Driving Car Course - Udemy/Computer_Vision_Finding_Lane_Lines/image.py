8851044687d3bf30b9a499884fd98a2fc17eda9c
Cornelio Posadas P <posadascornelio38@gmail.com>
2022-09-09 23:38:12 -0500

    dev canny image

--- The Complete Self Driving Car Course - Udemy/Computer_Vision_Finding_Lane_Lines/image.py
+++ The Complete Self Driving Car Course - Udemy/Computer_Vision_Finding_Lane_Lines/image.py
@@ -1,5 +1,12 @@
 import cv2
+import numpy as np
 
 image = cv2.imread(r"C:\Users\DELL\Desktop\Cursos\The self driving car course\The-Complete-Self-Driving-Car-Course\The Complete Self Driving Car Course - Udemy\Computer_Vision_Finding_Lane_Lines\Image\test_image.jpg")
-cv2.imshow('Result', image)
+lane_image = np.copy(image)
+gray = cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY)
+blur = cv2.GaussianBlur(gray, (5,5),0)
+canny = cv2.Canny(blur, 50, 150)
+
+cv2.imshow('Result', canny)
 cv2.waitKey(0)
+
