47c3ec7464b4f81ea68e9b303a01002bd0ae64b4
BernardoCanul <bcanul01@outlook.es>
2023-05-22 15:40:41 -0600

    try-except implemented

--- PIA SDV/LinetrackerV5_motores.py
+++ PIA SDV/LinetrackerV5_motores.py
@@ -4,11 +4,9 @@ import numpy as np
 cap = cv2.VideoCapture(0) 
 
 while True:
-    # ret, frame = cap.read()
-    # if not ret: break # break if no next frame
+    ret, frame = cap.read()
+    if not ret: break # break if no next frame
     
-    frame = cv2.imread("camino.jpeg")
-    frame = cv2.resize(frame, [300,400])
     frame = cv2.GaussianBlur(frame, (5,5), 0)
 
     # Threshold
@@ -20,25 +18,28 @@ while True:
     contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     cv2.drawContours(frame, contours, -1, [255,0,0], 10)
 
-    # Encontrando el punto medio de la línea
-    biggest_contour = max(contours, key = cv2.contourArea)
-    M = cv2.moments(biggest_contour)
+    try:
+        # Encontrando el punto medio de la línea
+        biggest_contour = max(contours, key = cv2.contourArea)
+        M = cv2.moments(biggest_contour)
 
-    if M["m00"] != 0:
-        cx = int(M["m10"] / M["m00"])
-        cy = int(M["m01"] / M["m00"])
-    else:
-        cx, cy = 0, 0
+        if M["m00"] != 0:
+            cx = int(M["m10"] / M["m00"])
+            cy = int(M["m01"] / M["m00"])
+        else:
+            cx, cy = 0, 0
 
-    ancho = frame.shape[1]
-    cx_cam = int(ancho/2)
-    x_offset = cx - cx_cam
-    x_centered = int(ancho/2) + x_offset
-    cv2.circle(frame, (cx, cy),15 , (255, 255, 0), -1)
+        ancho = frame.shape[1]
+        cx_cam = int(ancho/2)
+        x_offset = cx - cx_cam
+        x_centered = int(ancho/2) + x_offset
+        cv2.circle(frame, (cx, cy),15 , (255, 255, 0), -1)
 
-    # Calculando distancia del centro de la camara hacia la línea
-    distancia = cx - cx_cam
-    cv2.putText(frame, f"Distancia: {distancia} px", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2, cv2.LINE_AA)
+        # Calculando distancia del centro de la camara hacia la línea
+        distancia = cx - cx_cam
+        cv2.putText(frame, f"Distancia: {distancia} px", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2, cv2.LINE_AA)
+    except:
+        continue
     
     # Muestra el video en pantalla
     cv2.imshow('Frame', frame)
