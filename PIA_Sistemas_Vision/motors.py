448d8521303fc63727a9ed19f21903281afa10b9
Cornelio Posadas P <posadascornelio38@gmail.com>
2023-05-23 17:11:22 -0600

    add motors code again

--- PIA_Sistemas_Vision/motors.py
+++ PIA_Sistemas_Vision/motors.py
@@ -14,12 +14,22 @@ GPIO.setup(MotorIN2,GPIO.OUT)
 GPIO.setup(MotorIN3,GPIO.OUT)
 GPIO.setup(MotorIN4,GPIO.OUT)
 
-while True:
-
+def foward():
     GPIO.output(MotorIN2,GPIO.HIGH) # Establecemos el sentido de giro con los pines IN1 e IN2  
     GPIO.output(MotorIN4,GPIO.LOW)  # Establecemos el sentido de giro con los pines IN1 e IN2
-    sleep(5)
     GPIO.output(MotorIN1,GPIO.HIGH)   # Establecemos el sentido de giro con los pines IN1 e IN2   
-    GPIO.output(MotorIN3,GPIO.LOW)  # Establecemos el sentido de giro con los pines IN1 e IN2 
+    GPIO.output(MotorIN3,GPIO.LOW) 
+
+def backward():
+    GPIO.output(MotorIN2,GPIO.LOW) # Establecemos el sentido de giro con los pines IN1 e IN2  
+    GPIO.output(MotorIN4,GPIO.HIGH)  # Establecemos el sentido de giro con los pines IN1 e IN2
+    GPIO.output(MotorIN1,GPIO.LOW)   # Establecemos el sentido de giro con los pines IN1 e IN2   
+    GPIO.output(MotorIN3,GPIO.HIGH) 
+
+while True:
+    foward()
     sleep(5)
+    backward()
+    sleep(5)
+
 GPIO.cleanup()
