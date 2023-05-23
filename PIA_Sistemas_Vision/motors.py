6d64fc0ec7801f674073d6ac98f0e3b4617d9c59
Cornelio Posadas P <posadascornelio38@gmail.com>
2023-05-23 15:39:22 -0600

    make it for motors

--- PIA_Sistemas_Vision/motors.py
+++ PIA_Sistemas_Vision/motors.py
@@ -5,7 +5,6 @@ GPIO.setmode(GPIO.BCM)
 
 MotorIN1 = 12
 MotorIN2 = 13
-
 MotorIN3 = 18
 MotorIN4 = 19
 
@@ -13,14 +12,13 @@ GPIO.cleanup()
 
 GPIO.setup(MotorIN1,GPIO.OUT)
 GPIO.setup(MotorIN2,GPIO.OUT)
-
 GPIO.setup(MotorIN3,GPIO.OUT)
 GPIO.setup(MotorIN4,GPIO.OUT)
 
 print("Hacemos girar el motor en un sentido por 5 segundos")
-GPIO.output(MotorIN1,GPIO.HIGH) # Establecemos el sentido de giro con los pines IN1 e IN2  
-GPIO.output(MotorIN2,GPIO.HIGH)  # Establecemos el sentido de giro con los pines IN1 e IN2
+GPIO.output(MotorIN2,GPIO.HIGH) # Establecemos el sentido de giro con los pines IN1 e IN2  
+GPIO.output(MotorIN4,GPIO.LOW)  # Establecemos el sentido de giro con los pines IN1 e IN2
 
 print("Hacemos girar el motor en el sentido contrario por 5 segundos")
-GPIO.output(MotorIN3,GPIO.HIGH)   # Establecemos el sentido de giro con los pines IN1 e IN2   
-GPIO.output(MotorIN4,GPIO.HIGH)  # Establecemos el sentido de giro con los pines IN1 e IN2 
+GPIO.output(MotorIN1,GPIO.HIGH)   # Establecemos el sentido de giro con los pines IN1 e IN2   
+GPIO.output(MotorIN3,GPIO.LOW)  # Establecemos el sentido de giro con los pines IN1 e IN2 
