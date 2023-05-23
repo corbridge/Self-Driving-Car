7c3577f12749bc68a763c71eb9a7c2c8f6e50cb4
Cornelio Posadas P <posadascornelio38@gmail.com>
2023-05-23 17:19:02 -0600

    make left ans right functions

--- PIA_Sistemas_Vision/motors.py
+++ PIA_Sistemas_Vision/motors.py
@@ -15,21 +15,45 @@ GPIO.setup(MotorIN3,GPIO.OUT)
 GPIO.setup(MotorIN4,GPIO.OUT)
 
 def foward():
-    GPIO.output(MotorIN2,GPIO.HIGH) # Establecemos el sentido de giro con los pines IN1 e IN2  
-    GPIO.output(MotorIN4,GPIO.LOW)  # Establecemos el sentido de giro con los pines IN1 e IN2
-    GPIO.output(MotorIN1,GPIO.HIGH)   # Establecemos el sentido de giro con los pines IN1 e IN2   
-    GPIO.output(MotorIN3,GPIO.LOW) 
+    GPIO.output(MotorIN2,GPIO.HIGH)
+    GPIO.output(MotorIN4,GPIO.LOW)
+
+    GPIO.output(MotorIN1,GPIO.HIGH) 
+    GPIO.output(MotorIN3,GPIO.LOW)
 
 def backward():
-    GPIO.output(MotorIN2,GPIO.LOW) # Establecemos el sentido de giro con los pines IN1 e IN2  
-    GPIO.output(MotorIN4,GPIO.HIGH)  # Establecemos el sentido de giro con los pines IN1 e IN2
-    GPIO.output(MotorIN1,GPIO.LOW)   # Establecemos el sentido de giro con los pines IN1 e IN2   
-    GPIO.output(MotorIN3,GPIO.HIGH) 
+    GPIO.output(MotorIN2,GPIO.LOW)
+    GPIO.output(MotorIN4,GPIO.HIGH)
+
+    GPIO.output(MotorIN1,GPIO.LOW)
+    GPIO.output(MotorIN3,GPIO.HIGH)
+
+def left():
+    GPIO.output(MotorIN2,GPIO.LOW)
+    GPIO.output(MotorIN4,GPIO.HIGH)
+
+    GPIO.output(MotorIN1,GPIO.HIGH)
+    GPIO.output(MotorIN3,GPIO.LOW)
+
+def right():
+    GPIO.output(MotorIN2,GPIO.HIGH)
+    GPIO.output(MotorIN4,GPIO.LOW)
+
+    GPIO.output(MotorIN1,GPIO.LOW)
+    GPIO.output(MotorIN3,GPIO.HIGH)
 
 while True:
     foward()
+    print('ADELANTE')
     sleep(5)
     backward()
+    print('ATRAS')
+    sleep(5)
+    right()
+    print('DERECHA')
+    sleep(5)
+    left()
+    print('IZQUIERDA')
     sleep(5)
 
 GPIO.cleanup()
