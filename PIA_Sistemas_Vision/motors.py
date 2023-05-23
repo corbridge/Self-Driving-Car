56556bc597c60caa55235c16a372b80a47816859
Cornelio Posadas P <posadascornelio38@gmail.com>
2023-05-23 15:49:56 -0600

    add while true

--- PIA_Sistemas_Vision/motors.py
+++ PIA_Sistemas_Vision/motors.py
@@ -15,11 +15,11 @@ GPIO.setup(MotorIN2,GPIO.OUT)
 GPIO.setup(MotorIN3,GPIO.OUT)
 GPIO.setup(MotorIN4,GPIO.OUT)
 
-print("Hacemos girar el motor en un sentido por 5 segundos")
-GPIO.output(MotorIN2,GPIO.HIGH) # Establecemos el sentido de giro con los pines IN1 e IN2  
-GPIO.output(MotorIN4,GPIO.LOW)  # Establecemos el sentido de giro con los pines IN1 e IN2
-sleep(5)
-print("Hacemos girar el motor en el sentido contrario por 5 segundos")
-GPIO.output(MotorIN1,GPIO.HIGH)   # Establecemos el sentido de giro con los pines IN1 e IN2   
-GPIO.output(MotorIN3,GPIO.LOW)  # Establecemos el sentido de giro con los pines IN1 e IN2 
-sleep(5)
+while True:
+
+    GPIO.output(MotorIN2,GPIO.HIGH) # Establecemos el sentido de giro con los pines IN1 e IN2  
+    GPIO.output(MotorIN4,GPIO.LOW)  # Establecemos el sentido de giro con los pines IN1 e IN2
+    sleep(5)
+    GPIO.output(MotorIN1,GPIO.HIGH)   # Establecemos el sentido de giro con los pines IN1 e IN2   
+    GPIO.output(MotorIN3,GPIO.LOW)  # Establecemos el sentido de giro con los pines IN1 e IN2 
+    sleep(5)
