991fb37bb888744276cb2a63a4a29259d97f13a0
Cornelio Posadas P <posadascornelio38@gmail.com>
2023-05-23 15:35:10 -0600

    puts lot of cleanups

--- PIA_Sistemas_Vision/motors.py
+++ PIA_Sistemas_Vision/motors.py
@@ -14,17 +14,13 @@ GPIO.setup(MotorIN2,GPIO.OUT)
 
 GPIO.setup(MotorIN3,GPIO.OUT)
 GPIO.setup(MotorIN4,GPIO.OUT)
-
+GPIO.cleanup()
 print("Hacemos girar el motor en un sentido por 5 segundos")
 GPIO.output(MotorIN1,GPIO.HIGH) # Establecemos el sentido de giro con los pines IN1 e IN2  
 GPIO.output(MotorIN2,GPIO.HIGH)  # Establecemos el sentido de giro con los pines IN1 e IN2
-
-sleep(5)
-
+GPIO.cleanup()
 print("Hacemos girar el motor en el sentido contrario por 5 segundos")
 GPIO.output(MotorIN3,GPIO.HIGH)   # Establecemos el sentido de giro con los pines IN1 e IN2   
 GPIO.output(MotorIN4,GPIO.HIGH)  # Establecemos el sentido de giro con los pines IN1 e IN2 
-
-sleep(5)
 
 GPIO.cleanup()
